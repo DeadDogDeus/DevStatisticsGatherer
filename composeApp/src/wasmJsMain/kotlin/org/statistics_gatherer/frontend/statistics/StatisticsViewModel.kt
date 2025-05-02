304e9164621aa4c18add880f001441934064abbe
Oleksandr Bondarenko <aleksander.bondarenko@infobric.com>
2025-05-02 22:16:06 +0200

    add statistic by users

--- composeApp/src/wasmJsMain/kotlin/org/statistics_gatherer/frontend/statistics/StatisticsViewModel.kt
+++ composeApp/src/wasmJsMain/kotlin/org/statistics_gatherer/frontend/statistics/StatisticsViewModel.kt
@@ -22,43 +22,10 @@ data class UserPullRequests(
 class StatisticsViewModel(
     private val pullRequestService: PullRequestsService
 ): ViewModel() {
-    private val _allByYear: MutableStateFlow<List<PullRequestByYear>> = MutableStateFlow(listOf(
-        PullRequestByYear(2020, 100),
-        PullRequestByYear(2021, 200),
-        PullRequestByYear(2022, 190),
-        PullRequestByYear(2023, 300),
-        PullRequestByYear(2024, 400),
-        PullRequestByYear(2025, 100)
-    ))
-
+    private val _allByYear: MutableStateFlow<List<PullRequestByYear>> = MutableStateFlow(emptyList())
     val allByYear: List<PullRequestByYear> get() = _allByYear.value
 
-    private val _userPullRequests: MutableStateFlow<List<UserPullRequests>> = MutableStateFlow(listOf(
-        UserPullRequests("User1", listOf(
-            PullRequestByYear(2020, 10),
-            PullRequestByYear(2021, 20),
-            PullRequestByYear(2022, 30),
-            PullRequestByYear(2023, 40),
-            PullRequestByYear(2024, 50),
-            PullRequestByYear(2025, 0)
-        )),
-        UserPullRequests("User2", listOf(
-            PullRequestByYear(2020, 15),
-            PullRequestByYear(2021, 25),
-            PullRequestByYear(2022, 35),
-            PullRequestByYear(2023, 10),
-            PullRequestByYear(2024, 17),
-            PullRequestByYear(2025, 65)
-        )),
-        UserPullRequests("User3", listOf(
-            PullRequestByYear(2020, 0),
-            PullRequestByYear(2021, 0),
-            PullRequestByYear(2022, 0),
-            PullRequestByYear(2023, 0),
-            PullRequestByYear(2024, 55),
-            PullRequestByYear(2025, 65)
-        ))
-    ))
+    private val _userPullRequests: MutableStateFlow<List<UserPullRequests>> = MutableStateFlow(emptyList())
     val userPullRequests: List<UserPullRequests> get() = _userPullRequests.value
 
     fun initViewModel() {
@@ -73,6 +40,29 @@ class StatisticsViewModel(
                 _allByYear.value = allByYear.map { (year, count) ->
                     PullRequestByYear(year, count)
                 }.sortedBy { it.year }
+
+                val users = mutableMapOf<String, MutableMap<Int, Int>>()
+
+                pullRequests.forEach { pullRequest ->
+                    val user = pullRequest.author.displayName
+                    users[user] = users[user] ?: mutableMapOf()
+                    users[user]!![pullRequest.year] = (users[user]!![pullRequest.year] ?: 0) + 1
+                }
+
+                users.forEach {
+                    allByYear.keys.forEach { year ->
+                        it.value[year] = it.value[year] ?: 0
+                    }
+                }
+
+                _userPullRequests.value = users.map { (user, years) ->
+                    UserPullRequests(
+                        user,
+                        years.map { (year, count) ->
+                            PullRequestByYear(year, count)
+                        }.sortedBy { it.year }
+                    )
+                }.sortedBy { it.user }
             }
         }
     }
